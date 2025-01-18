0e1f641309ff50065872f867dd96e96643b405ed
bayuhusada <nekobay08@gmail.com>
2025-01-18 21:56:09 +0800

    hero

--- components/Hero/HeroHeader.jsx
+++ components/Hero/HeroHeader.jsx
@@ -3,15 +3,37 @@ import React from 'react'
 
 import header from '../../assets/ladies_header.jpg'
 import headerKana from '../../assets/ladies_kanan.jpg'
+import Link from 'next/link'
 
 const HeroHeader = () => {
   return (
     <div>
-      <Image src={header} className='w-full' alt='header' />
-      <div>
-      <video src="../../assets/viedeo.mp4" controls></video>
-      <Image src={headerKana} alt="alt"/>
+      <div className=''>
+        <Image src={header} className='w-full absolute' alt='header' />
+        <div className='flex justify-between m-5'>
+        <p className='relative top-[750px] ml-10 text-white text-5xl uppercase font-semibold'>happy lunar new year</p>
+        <p className='relative top-[750px] ml-10 text-black bg-white p-3  uppercase font-semibold'>buy now</p>
+        </div>
       </div>
+      <div className='flex flex-row w-full relative top-[800px]'>
+        <div className='h-full w-1/2'>
+          <video src="https://acc-media-assets.s3.ap-southeast-1.amazonaws.com/hm_id/vmd/24/NS/12/1000-4x5-women-startpage-wk37.mp4" controls autoPlay={true} loop></video>
+          <div className='flex justify-between mx-11 my-5'>
+            <p className='text-4xl uppercase font-semibold'>New Arrivals</p>
+            <p className='text-base font-semibold uppercase '>buy now</p>
+          </div>
+        </div>
+        <div className='h-1/2 w-1/2'>
+          <Link href="/">
+            <Image src={headerKana} alt="alt" />
+          </Link>
+          <div className='flex justify-between mx-11 my-5'>
+            <p className='text-4xl uppercase font-semibold'>New season preppy</p>
+            <p className='text-base font-semibold uppercase '>buy now</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- components/Navbar/HeaderBar.jsx
+++ components/Navbar/HeaderBar.jsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { Bag, CaretDown, Equals, Globe, Heart, MagnifyingGlass, Minus, User } from '@phosphor-icons/react/dist/ssr'
+import { Bag, CaretDown, Equals, Globe, Heart, MagnifyingGlass, Minus, User, X } from '@phosphor-icons/react/dist/ssr'
 
 import logo from '@/assets/logo.svg'
-import SideBar from './SideBar'
 const HeaderBar = () => {
 
   const [open, setOpen] = useState(false)
 
   return (
-    
+  <>
     <div className='flex items-center justify-between'>
       <div className='flex m-5 gap-10 '>
       <Image src={logo} alt="logo" width={50} height={50} />
@@ -39,8 +38,31 @@ const HeaderBar = () => {
           <CaretDown size={20} />
         </span>
       </div>
-      {open && <SideBar /> }
     </div>
+{open && 
+<div>
+    <div className='fixed top-0 left-0 h-full bg-white text-white p-5 transform transition-transform duration-300 ease-in-out z-10'>
+      <div className="flex text-black items-center justify-center gap-8 m-3">
+      <button
+      onClick={() => setOpen(!open)}>
+          <X weight='bold'size={20} className='text-black' />
+        </button>
+      <Link href="/" className='uppercase font-semibold '>ladies</Link>
+      <Link href="/" className='uppercase text-gray-500 font-semibold '>MEN</Link>
+      <Link href="/" className='uppercase text-gray-500 font-semibold '>BABY</Link>
+      <Link href="/" className='uppercase text-gray-500 font-semibold '>KIDS</Link>
+      <Link href="/" className='uppercase text-gray-500 font-semibold '>SPORT</Link>
+      </div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+</div>
+  }
+
+  </>
     
   )
 }
